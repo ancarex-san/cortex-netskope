bbeea5e28c4160b969b9afc3d390e34047f3ae05
ancarex-san <66373605+ancarex-san@users.noreply.github.com>
2024-07-23 17:11:29 -0500

    Update palo_alto_networks_cortex_xdr_constants.py

--- utils/palo_alto_networks_cortex_xdr_constants.py
+++ utils/palo_alto_networks_cortex_xdr_constants.py
@@ -1,6 +1,6 @@
 # Constants for Palo Alto Networks Cortex XDR integration
 MODULE_NAME = "Palo Alto Networks Cortex XDR incident"
-PLATFORM_NAME = "Cortex XDR"
+PLATFORM_NAME = "Cortex XDR incidents"
 PLUGIN_NAME = "CortexXDRIncidents"
 PLUGIN_VERSION = "1.0.0"
 PLUGIN_CHECKPOINT = "cortex_xdr_checkpoint"
