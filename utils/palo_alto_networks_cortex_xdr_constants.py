8ab779f6d34ec8269a4a80e65cae655447ce810b
ancarex-san <66373605+ancarex-san@users.noreply.github.com>
2024-07-23 20:56:08 -0500

    Update palo_alto_networks_cortex_xdr_constants.py

--- utils/palo_alto_networks_cortex_xdr_constants.py
+++ utils/palo_alto_networks_cortex_xdr_constants.py
@@ -1,23 +1,7 @@
-# Constants for Palo Alto Networks Cortex XDR integration
-MODULE_NAME = "Palo Alto Networks Cortex XDR incident"
-PLATFORM_NAME = "Cortex XDR incidents"
-PLUGIN_NAME = "CortexXDRIncidents"
+# Constants for Cortex XDR Incidents Plugin integration
+MODULE_NAME = "Cortex XDR Incidents Plugin"
+PLATFORM_NAME = "Cortex XDR"
+PLUGIN_NAME = "CortexXDRIncidentsPlugin"
 PLUGIN_VERSION = "1.0.0"
-PLUGIN_CHECKPOINT = "cortex_xdr_checkpoint"
+PLUGIN_CHECKPOINT = "cortex_xdr_incidents_plugin_checkpoint"
 FETCH_INTERVAL = 60  # Interval in seconds to fetch incidents
-
-CORTEX_TO_CE_SEVERITY_MAPPING = {
-    "high": "High",
-    "medium": "Medium",
-    "low": "Low",
-    "informational": "Informational",
-}
-
-DEFAULT_BATCH_SIZE = 200
-INTERNAL_SEVERITY_TO_CORTEX = {
-    "High": "high",
-    "Medium": "medium",
-    "Low": "low",
-    "Informational": "informational",
-}
-
