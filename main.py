1dde2f2cd69947f383822adcac7a67155c153f21
ancarex-san <66373605+ancarex-san@users.noreply.github.com>
2024-07-23 20:50:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,51 @@
 """
 BSD 3-Clause License
 
-Copyright (c) 2021, Netskope OSS
-All rights reserved.
+[License text omitted for brevity]
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-CTE Palo Alto Networks Cortex XDR plugin's main file which contains
-the implementation of all the plugin's methods.
+CTE Cortex XDR Incidents Plugin main file which contains the implementation of all the plugin's methods.
 """
+import datetime
+import hashlib
+import ipaddress
 import json
 import os
-import time
-from typing import List, Optional, Tuple
-from cortex_xdr_client.api.authentication import Authentication
-from cortex_xdr_client.api.base_api import BaseAPI
-from cortex_xdr_client.api.models.filters import (
-    new_request_data,
-    request_eq_neq_filter,
+import re
+import secrets
+import string
+import traceback
+from typing import Dict, List, Tuple
+
+from urllib.parse import urlparse
+from netskope.integrations.cte.models import (
+    Indicator,
+    IndicatorType,
+    SeverityType,
 )
-from cortex_xdr_client.api.models.incidents import (
-    GetExtraIncidentDataResponse,
-    GetIncidentsResponse,
+from netskope.integrations.cte.models.business_rule import (
+    Action,
+    ActionWithoutParams,
 )
-from netskope.integrations.cte.plugin_base import PluginBase
+from netskope.integrations.cte.models.tags import TagIn
+from netskope.integrations.cte.plugin_base import (
+    PluginBase,
+    PushResult,
+    ValidationResult,
+)
 from netskope.integrations.cte.utils import TagUtils
-from palo_alto_networks_cortex_xdr_constants import (
-    MODULE_NAME, PLATFORM_NAME, PLUGIN_NAME, PLUGIN_VERSION, PLUGIN_CHECKPOINT
+from pydantic import ValidationError
+
+from cortex_xdr_incidents_plugin_constants import (
+    MODULE_NAME,
+    PLATFORM_NAME,
+    PLUGIN_CHECKPOINT,
+    PLUGIN_NAME,
+    PLUGIN_VERSION,
 )
-from palo_alto_networks_cortex_xdr_helper import PaloAltoCortexNetworksXDRPluginHelper
+from cortex_xdr_incidents_plugin_helper import (
+    CortexXDRIncidentsPluginHelper,
+    PaloAltoCortexNetworksXDRPluginException,
+)
 
 # Obtener la configuración del manifest
 def get_plugin_config():
@@ -97,14 +91,14 @@ class IncidentsAPI(BaseAPI):
 
 incidents_api = IncidentsAPI(auth, fqdn, timeout)
 
-class PaloAltoNetworksCortexXDRPlugin(PluginBase):
+class CortexXDRIncidentsPlugin(PluginBase):
     def __init__(self, name, *args, **kwargs):
         super().__init__(name, *args, **kwargs)
         self.plugin_name, self.plugin_version = self._get_plugin_info()
         self.log_prefix = f"{MODULE_NAME} {self.plugin_name}"
         if name:
             self.log_prefix = f"{self.log_prefix} [{name}]"
-        self.palo_alto_cortex_helper = PaloAltoCortexNetworksXDRPluginHelper(
+        self.cortex_xdr_helper = CortexXDRIncidentsPluginHelper(
             logger=self.logger,
             log_prefix=self.log_prefix,
             plugin_name=self.plugin_name,
@@ -142,10 +136,85 @@ class PaloAltoNetworksCortexXDRPlugin(PluginBase):
         while True:
             incidents = self.fetch_incidents()
             for incident in incidents:
-                self.palo_alto_cortex_helper.process_incident(incident)
-                # Fetch and process extra data for each incident if needed
+                self.cortex_xdr_helper.process_incident(incident)
                 extra_data = self.get_incident_extra_data(incident['incident_id'], alerts_limit=10)
                 if extra_data:
-                    # Procesar datos adicionales
                     self.logger.info(f"Extra data for incident {incident['incident_id']}: {extra_data}")
             time.sleep(fetch_interval)
+
+    def _validate_auth_params(self, configuration) -> ValidationResult:
+        """Validate the authentication params with Palo Alto Cortex XDR platform."""
+        try:
+            self.logger.debug(f"{self.log_prefix}: Validating auth credentials.")
+            base_url = configuration.get("base_url", "").strip().strip("/")
+            headers = self._authorize_request(configuration=configuration)
+            payload = json.dumps({
+                "request_data": {
+                    "filters": [],
+                    "search_from": 0,
+                    "search_to": 1,
+                }
+            })
+            url = f"{base_url}/public_api/v1/incidents/get_incidents/"
+            logger_msg = "validating auth credentials"
+            response = self.cortex_xdr_helper.api_helper(
+                url=url,
+                method="POST",
+                headers=headers,
+                data=payload,
+                proxies=self.proxy,
+                verify=self.ssl_validation,
+                is_handle_error_required=False,
+                logger_msg=logger_msg,
+            )
+            if response.status_code == 200:
+                self.logger.debug(f"{self.log_prefix}: Successfully validated auth credentials and plugin configuration.")
+                return ValidationResult(success=True, message=f"Validation successful for {PLUGIN_NAME} plugin configuration.")
+            elif response.status_code == 400:
+                err_msg = "Received exit code 400. Resource not found. Verify FQDN Key provided in the configuration parameters."
+                self.logger.error(message=f"{self.log_prefix}: {err_msg}", details=str(response.text))
+                return ValidationResult(success=False, message=err_msg)
+            elif response.status_code == 401:
+                err_msg = "Received exit code 401, Unauthorized access. Verify API Key, API Key ID and Authentication Method provided in the configuration parameters."
+                self.logger.error(message=f"{self.log_prefix}: {err_msg}", details=str(response.text))
+                return ValidationResult(success=False, message=err_msg)
+            elif response.status_code == 403:
+                err_msg = "Received exit code 403, Forbidden access. Verify API Key provided in the configuration parameters."
+                self.logger.error(message=f"{self.log_prefix}: {err_msg}", details=str(response.text))
+                return ValidationResult(success=False, message=err_msg)
+            self.cortex_xdr_helper.handle_error(resp=response, logger_msg=logger_msg)
+        except PaloAltoCortexNetworksXDRPluginException as exp:
+            err_msg = f"Validation error occurred. Error: {exp}"
+            self.logger.error(message=f"{self.log_prefix}: {err_msg}", details=str(traceback.format_exc()))
+            return ValidationResult(success=False, message=str(exp))
+        except Exception as exp:
+            err_msg = "Unexpected validation error occurred."
+            self.logger.error(message=f"{self.log_prefix}: {err_msg} Error: {exp}", details=str(traceback.format_exc()))
+            return ValidationResult(success=False, message=f"{err_msg} Check logs for more details.")
+
+    def _authorize_request(self, configuration: Dict) -> Dict:
+        """Authorize request on the basis of Authentication Method."""
+        headers = {"Content-Type": "application/json"}
+        auth_method = configuration.get("auth_method")
+        api_key = configuration.get("api_key")
+        api_key_id = configuration.get("api_key_id")
+        if auth_method == "standard":
+            headers.update({"x-xdr-auth-id": str(api_key_id), "Authorization": api_key})
+            return headers
+        elif auth_method == "advanced":
+            nonce = "".join([secrets.choice(string.ascii_letters + string.digits) for _ in range(64)])
+            timestamp = int(datetime.datetime.now(datetime.timezone.utc).timestamp()) * 1000
+            auth_key = "%s%s%s" % (api_key, nonce, timestamp)
+            auth_key = auth_key.encode("utf-8")
+            api_key_hash = hashlib.sha256(auth_key).hexdigest()
+            headers.update({
+                "x-xdr-timestamp": str(timestamp),
+                "x-xdr-nonce": nonce,
+                "x-xdr-auth-id": str(api_key_id),
+                "Authorization": api_key_hash,
+            })
+            return headers
+        else:
+            err_msg = "Invalid Authentication Method found in the configuration parameters."
+            self.logger.error(f"{self.log_prefix}: {err_msg}")
+            raise PaloAltoCortexNetworksXDRPlugin
